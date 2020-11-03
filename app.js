174d0255251656402dc873d23f8f1761ddc194eb
Amee Parmar <amee05@yahoo.com>
2020-11-03 10:30:20 -0800

    code

---
+++ app.js
@@ -0,0 +1,17 @@
+const startBtn = document.getElementById(start)
+startBtn.addEventListener('click', startQuiz)
+
+
+
+function startQuiz() {
+  console.log ('started')
+}
+
+function nextQuestion() {
+  
+}
+
+
+function showAnswer() {
+  
+}
